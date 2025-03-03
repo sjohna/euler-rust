ac879cd96258968ef71ac124d8c6e0d7136df25f
John Stevens <john.allen.stevens@fastmail.com>
2025-03-03 09:22:17 -0700

    Add implementation of problem 1 based on std::iter::from_fn and custom function

--- src/euler1.rs
+++ src/euler1.rs
@@ -16,6 +16,19 @@ pub fn euler1_iterator() -> i32 {
     (1..1000).filter(|n| n % 3 == 0 || n % 5 == 0).sum()
 }
 
+#[allow(dead_code)]
+pub fn euler1_custom_iterator_function() -> i32 {
+    std::iter::from_fn(natural_numbers()).take_while(|n| n < &1000).filter(|n| n % 3 == 0 || n % 5 == 0).sum()  // what is the & I need before the literal 1000 in the take_while?
+}
+
+fn natural_numbers() -> impl FnMut() -> Option<i32> {   // this needs to be FnMut instead of Fn. What's the difference?
+    let mut num = 1;
+    move || -> Option<i32> {    // I need `move` here or I get heinous compiler errors. What's this doing?
+        num += 1;
+        Some(num)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -26,5 +39,10 @@ mod tests {
     #[test]
     fn euler1_iterator() {
         assert_eq!(super::euler1_iterator(), 233168);
+    }
+
+    #[test]
+    fn euler1_custom_iterator_function() {
+        assert_eq!(super::euler1_custom_iterator_function(), 233168);
     }
 }
