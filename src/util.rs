654ef084e0d3409e3ed25ddd8faa2511acda89ee
John Stevens <john.allen.stevens@fastmail.com>
2025-03-08 15:35:42 -0700

    Move root decimal digit sum into util

--- src/util.rs
+++ src/util.rs
@@ -74,6 +74,45 @@ pub fn factorial(n: i32) -> BigInt {
     fac
 }
 
+pub fn root_digit_sum(n: i32, digits: i32) -> i32 {
+    let mut root = BigInt::from(0);
+
+    for magnitude in 0..digits {
+        let target = BigInt::from(n) * BigInt::from(10).pow((2 * magnitude) as u32);
+        root *= 10;
+
+        // binary search to find the next digit
+        let mut min = 0;
+        let mut max = 9;
+        while max - min > 0 {
+            let next_digit = (max + min + 1) / 2;
+            let candidate = &root + next_digit; // why do I need a & on root here?
+
+            let candidate_squared = &candidate * &candidate;
+
+            if candidate_squared > target {   // and here?
+                max = next_digit-1;
+            } else {
+                min = next_digit;
+            }
+        }
+
+        root += min;
+    }
+
+    digit_sum(root)
+}
+
+pub fn digit_sum(n: BigInt) -> i32 {
+    let str = n.to_string();   // why the second toString() here?
+    let mut sum = 0;
+    for digit in str.chars() {
+        sum += digit as i32 - '0' as i32;
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
