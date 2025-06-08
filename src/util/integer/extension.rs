87252f6ad2e7bed2c9b0e0d5a76b25104fb610bc
John Stevens <john.allen.stevens@fastmail.com>
2025-06-08 14:36:07 -0600

    Util: IntegerExt methods digit_sum and sorted_digit_string

--- src/util/integer/extension.rs
+++ src/util/integer/extension.rs
@@ -1,10 +1,13 @@
-use std::ops::{DivAssign, Rem};
+use std::ops::{AddAssign, DivAssign, Rem};
 
 pub trait IntegerExt {
-     fn divide_out(&self, divisor: Self) -> Self;
+    fn divide_out(&self, divisor: Self) -> Self;
+    fn digit_sum(&self) -> Self;
+    fn sorted_digit_string(&self) -> String;
  }
 
- impl <I> IntegerExt for I where I: DivAssign + Rem<Output = Self> + From<u8> + Copy + PartialEq {
+// TODO: way to make these generic type descriptions better?
+ impl <I> IntegerExt for I where I: DivAssign + Rem<Output = Self> + AddAssign + From<u8> + Copy + PartialEq + Default + ToString {
      fn divide_out(&self, divisor: Self) -> Self {
          let mut result = *self;
 
@@ -14,6 +17,26 @@ pub trait IntegerExt {
 
          result
      }
+
+     fn digit_sum(&self) -> Self {
+         let mut running_value = *self;
+         let mut result = Self::default();
+         let ten = Self::from(10);
+
+         while running_value != 0.into() {
+             result += running_value % ten;
+             running_value /= ten;
+         }
+
+         result
+     }
+
+    fn sorted_digit_string(&self) -> String {
+        let mut ret = self.to_string().chars().collect::<Vec<_>>();
+        ret.sort_unstable();
+
+        ret.into_iter().collect()
+    }
  }
 
 #[cfg(test)]
@@ -23,5 +46,19 @@ mod tests {
     fn test_divide_out() {
         assert_eq!(12_i32.divide_out(3), 4);
         assert_eq!(24_i64.divide_out(2), 3);
+    }
+
+    #[test]
+    fn test_digit_sum() {
+        assert_eq!(12_i32.digit_sum(), 3);
+        assert_eq!(24_i64.digit_sum(), 6);
+        assert_eq!(102030405_u64.digit_sum(), 15);
+    }
+
+    #[test]
+    fn test_sorted_digit_string() {
+        assert_eq!(132_i32.sorted_digit_string(), "123");
+        assert_eq!(908070605_i32.sorted_digit_string(), "000056789");
+
     }
 }
