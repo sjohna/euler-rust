54cf41dd2d9ea4689a082ff6fe912a351e84e5f3
John Stevens <john.allen.stevens@fastmail.com>
2025-06-08 14:49:35 -0600

    Use f64::total_cmp on Problem 69

--- src/problems/euler69.rs
+++ src/problems/euler69.rs
@@ -6,5 +6,5 @@ pub fn sieve(n: i64) -> i64 {
 
     let totients = totient::sieve_up_to(n, &primes);
 
-    totients.iter().enumerate().skip(1).map(|(i,v)| (i, i as f64 / *v as f64)).max_by(|a, b| a.1.partial_cmp(&b.1).unwrap()).unwrap().0 as i64
+    totients.iter().enumerate().skip(1).map(|(i,v)| (i, i as f64 / *v as f64)).max_by(|a, b| f64::total_cmp(&a.1, &b.1)).unwrap().0 as i64
 }
