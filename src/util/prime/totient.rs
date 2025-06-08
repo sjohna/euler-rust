a6dd13fb138c9fe3de5ae06f3528d14112829cee
John Stevens <john.allen.stevens@fastmail.com>
2025-06-08 14:01:31 -0600

    Util: totient sieve

--- src/util/prime/totient.rs
+++ src/util/prime/totient.rs
@@ -23,13 +23,31 @@ pub fn from_prime_factorization(mut n: i64, prime_factorization: &[(i64, u32)]) 
     totient
 }
 
+pub fn sieve_up_to(n: i64, primes: &[i64]) -> Vec<i64> {
+    let mut ret = vec![];
+
+    ret.reserve(n as usize + 1);
+
+    for i in 0..=n {
+        ret.push(i);
+    }
+
+    for p in primes {
+        for i in (*p..=n).step_by(*p as usize) {
+            ret[i as usize] -= ret[i as usize] / p;
+        }
+    }
+
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     use crate::util::prime;
-    use super::{brute_force, from_prime_factorization};
+    use super::{brute_force, from_prime_factorization, sieve_up_to};
 
     #[test]
-    fn test_totient_from_prime_factorization() {
+    fn test_from_prime_factorization() {
         let primes = prime::seq::sieve_up_to(5_000_000).collect::<Vec<_>>();
 
         assert_eq!(from_prime_factorization(1, &prime::prime_factorization(1, &primes)), 1);
@@ -47,7 +65,7 @@ mod tests {
     }
 
     #[test]
-    fn test_totient_brute_force() {
+    fn test_brute_force() {
         assert_eq!(brute_force(1), 1);
         assert_eq!(brute_force(2), 1);
         assert_eq!(brute_force(3), 2);
@@ -60,5 +78,16 @@ mod tests {
 
         assert_eq!(brute_force(499998), 165336);
         assert_eq!(brute_force(4999980), 1322688);
+    }
+
+    #[test]
+    fn test_sieve_up_to() {
+        let primes = prime::seq::sieve_up_to(5_000_000).collect::<Vec<_>>();
+
+        assert_eq!(sieve_up_to(9, &primes), vec![0,1,1,2,2,4,2,6,4,6]);
+
+        let full_sieve = sieve_up_to(5_000_000, &primes);
+        assert_eq!(full_sieve[499998], 165336);
+        assert_eq!(full_sieve[4999980], 1322688);
     }
 }
